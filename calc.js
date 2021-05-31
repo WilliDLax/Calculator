c9c443f2ab1498f115ba71dc755ec7d01ae50295
William <shalomwill98@gmail.com>
2021-05-31 14:28:48 +0100

    made code simpler

--- calc.js
+++ calc.js
@@ -36,54 +36,46 @@ let answer;
 
 let display = document.querySelector(".screen");
 
-let numbers = document.querySelectorAll(".number");
-numbers.forEach(number => {
-    number.addEventListener("click", ()=>{
-        if(display.textContent == "0"){
-            display.textContent = "";
-        }
-        if(display.textContent == answer){display.textContent = "";}
-        display.textContent += number.textContent;
-    });
-}); 
+function showNumber(num){
+    if(display.textContent == "0"){
+        display.textContent = "";
+    }
+    if(display.textContent == answer){display.textContent = "";}
+    display.textContent += num.textContent;
+}
 
-let operators = document.querySelectorAll(".operator");
-operators.forEach(operator => {
-    operator.addEventListener("click", () => {
-        if(isNaN(parseInt(display.textContent[display.textContent.length-1]))){
-            return;
-        }
-        else if(firstNumber){
-            secondNumber = Number(display.textContent.slice((display.textContent.indexOf(sign)+1)));
-            let temp = operate(sign, firstNumber, secondNumber);
-            firstNumber = temp;
-            sign = operator.textContent;
-            display.textContent = firstNumber + sign;
-        }
-        else{
-            firstNumber = Number(display.textContent);
-            sign = operator.textContent;
-            display.textContent += sign;
-        }
-    });
-});
+function showOperator(ope){
+    if(isNaN(parseInt(display.textContent[display.textContent.length-1]))){
+        return;
+    }
+    else if(firstNumber){
+        secondNumber = Number(display.textContent.slice((display.textContent.indexOf(sign)+1)));
+        let temp = operate(sign, firstNumber, secondNumber);
+        firstNumber = temp;
+        sign = ope.textContent;
+        display.textContent = firstNumber + sign;
+    }
+    else{
+        firstNumber = Number(display.textContent);
+        sign = ope.textContent;
+        display.textContent += sign;
+    }
+}
 
-let dot = document.querySelector(".point");
-dot.addEventListener("click", () => {
-    if(display.textContent.includes(dot.textContent)){
+function showDot(d){
+    if(display.textContent.includes(d.textContent)){
         if(sign){
-            if((display.textContent.slice((display.textContent.indexOf(sign)+1))).includes(dot.textContent)){
+            if((display.textContent.slice((display.textContent.indexOf(sign)+1))).includes(d.textContent)){
                 return;
             }
-            else{display.textContent += dot.textContent;}
+            else{display.textContent += d.textContent;}
         }
         else{return;}
     }
-    else{display.textContent += dot.textContent;}
-});
+    else{display.textContent += d.textContent;}
+}
 
-let equal = document.querySelector(".equal");
-equal.addEventListener("click", () => {
+function showAnswer(){
     if((firstNumber || firstNumber == "0") && sign){
         if(display.textContent.slice((display.textContent.indexOf(sign)+1))){
             secondNumber = Number(display.textContent.slice((display.textContent.indexOf(sign)+1)));
@@ -97,22 +89,42 @@ equal.addEventListener("click", () => {
         }
         else return;
     }
-});
+}
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", () => {
+function clearAll(){
     display.textContent = "";
     firstNumber = undefined;
     secondNumber = undefined;
     sign = undefined;
     answer = undefined;
-});
+}
 
-const del = document.querySelector("#delete");
-del.addEventListener("click", () => {
+function backspace(){
     display.textContent = display.textContent.replace(display.textContent[display.textContent.length-1],"");
     if(display.textContent == ""){
         display.textContent = "";
     }
+}
+
+let numbers = document.querySelectorAll(".number");
+numbers.forEach(number => {
+    number.addEventListener("click", ()=>{showNumber(number)});
+}); 
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {showOperator(operator)});
 });
+
+let dot = document.querySelector(".point");
+dot.addEventListener("click", () => {showDot(dot)});
+
+let equal = document.querySelector(".equal");
+equal.addEventListener("click", showAnswer);
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", clearAll);
+
+const del = document.querySelector("#delete");
+del.addEventListener("click", backspace);
 
