25bd50c5b10994f8962887d9100e68434bcb1a60
William <shalomwill98@gmail.com>
2021-06-01 11:29:17 +0100

    Added keyboard support

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//Main calculation functions
 function add(number1, number2){
     return Number(number1) + Number(number2);
 }
@@ -20,7 +21,7 @@ function operate(operator, number1, number2){
             return add(number1,number2);
         case '-':
             return subtract(number1,number2);
-        case 'x':
+        case '*':
             return multiply(number1,number2);
         case '/':
             return divide(number1,number2);
@@ -36,12 +37,13 @@ let answer;
 
 let display = document.querySelector(".screen");
 
+//functions that choose what to send to the display
 function showNumber(num){
     if(display.textContent == "0"){
         display.textContent = "";
     }
     if(display.textContent == answer){display.textContent = "";}
-    display.textContent += num.textContent;
+    display.textContent += num;
 }
 
 function showOperator(ope){
@@ -52,27 +54,27 @@ function showOperator(ope){
         secondNumber = Number(display.textContent.slice((display.textContent.indexOf(sign)+1)));
         let temp = operate(sign, firstNumber, secondNumber);
         firstNumber = temp;
-        sign = ope.textContent;
+        sign = ope;
         display.textContent = firstNumber + sign;
     }
     else{
         firstNumber = Number(display.textContent);
-        sign = ope.textContent;
+        sign = ope;
         display.textContent += sign;
     }
 }
 
 function showDot(d){
-    if(display.textContent.includes(d.textContent)){
+    if(display.textContent.includes(d)){
         if(sign){
-            if((display.textContent.slice((display.textContent.indexOf(sign)+1))).includes(d.textContent)){
+            if((display.textContent.slice((display.textContent.indexOf(sign)+1))).includes(d)){
                 return;
             }
-            else{display.textContent += d.textContent;}
+            else{display.textContent += d;}
         }
         else{return;}
     }
-    else{display.textContent += d.textContent;}
+    else{display.textContent += d;}
 }
 
 function showAnswer(){
@@ -106,18 +108,19 @@ function backspace(){
     }
 }
 
+//handling click events from the user
 let numbers = document.querySelectorAll(".number");
 numbers.forEach(number => {
-    number.addEventListener("click", ()=>{showNumber(number)});
+    number.addEventListener("click", ()=>{showNumber(number.textContent)});
 }); 
 
 let operators = document.querySelectorAll(".operator");
 operators.forEach(operator => {
-    operator.addEventListener("click", () => {showOperator(operator)});
+    operator.addEventListener("click", () => {showOperator(operator.textContent)});
 });
 
 let dot = document.querySelector(".point");
-dot.addEventListener("click", () => {showDot(dot)});
+dot.addEventListener("click", () => {showDot(dot.textContent)});
 
 let equal = document.querySelector(".equal");
 equal.addEventListener("click", showAnswer);
@@ -128,3 +131,22 @@ clear.addEventListener("click", clearAll);
 const del = document.querySelector("#delete");
 del.addEventListener("click", backspace);
 
+//adding keyboard support
+document.querySelector("body").addEventListener("keydown", (pressed) => {
+    if(!isNaN(pressed.key)){
+        showNumber(pressed.key);
+    }
+    else if(pressed.key == "."){
+        showDot(pressed.key);
+    }
+    else if(pressed.key == "+" || pressed.key == "-" || pressed.key == "*" || pressed.key == "/"){
+        showOperator(pressed.key);
+    }
+    else if(pressed.key == "=" || pressed.key == "Enter"){
+        showAnswer();
+    }
+    else if(pressed.key == "Backspace"){
+        backspace();
+    }
+    else return;
+});
